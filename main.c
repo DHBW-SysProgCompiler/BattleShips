25a212ddba5a350ed97b71967ccd339ba7fa84b2
Erix <e.a.erath@gmail.com>
2024-02-28 11:48:52 +0100

    builds, runs and looks beautyful (code and game)

--- main.c
+++ main.c
@@ -10,12 +10,19 @@
  * @brief *Main* Entry
  **/
 
+#pragma region includes
+
+#include "modules/asciiart.h"
 #include "modules/board.h"
 #include "modules/print_boards.h"
 #include "modules/random.h"
 #include "modules/register_access.h"
 #include "modules/term_control.h"
 #include "modules/timer.h"
+
+#pragma endregion
+
+#pragma region globs
 
 board player;
 board bot;
@@ -27,12 +34,18 @@ uint8_t print_pls = 0;
 uint8_t check_game_over = 0;
 int victory = 0;
 
+#pragma endregion
+
+#pragma region support_methods
+
+// set the timer for the next shot of the bot
 void bot_scedule_next_shot() {
   const int tenth_sec = 50;
   int rand = math_mod(rng_getRandomValue_immediately(), 4);
   timer0_CC0_set_delay((rand + 1) * tenth_sec * difficulty);
 }
 
+// methot to be called by the timer-interrupt
 void bot_action() {
   uint8_t temp = board_bot_shoot(&player);
   print_pls = temp;
@@ -40,22 +53,25 @@ void bot_action() {
   bot_scedule_next_shot();
 }
 
-int main(void) {
-  // first print some ascii-art, so the player knows stuff is happening
-  term_init(0, 0);
-  term_clear_screen();
-  term_println("//start-ascii-art");
-
+// wraps all nessecasy inits and setups the boards
+void main_init() {
   // init all the shit
+  term_init(0, 0);
   rng_init();
   timer_init(TIMER0, 15, 3, 2397, bot_action);
   bot_scedule_next_shot();
 
+  // first print some ascii-art, so the player knows stuff is happening
+  term_clear_screen();
+  art_print_main();
+
   // place the ships an both boards
   board_place_ships(&player);
   board_place_ships(&bot);
+}
 
-  // now that everything is ready we tell the player to start the game
+// interupt execution until dificulty is selected
+void select_dificulty() {
   term_print("\n\nselect dificulty;\n[1] = hard\n[2] = medim (default)\n[3] = easy");
   while (1) {
     char c = term_stdin_read();
@@ -66,8 +82,11 @@ int main(void) {
       break;
     }
   }
+}
 
-  // start the game
+// main gameplay loop
+void main_game_loop() {
+
   term_clear_screen();
   print_boards(&player, &bot, &cursor);
   timer_start(TIMER0);
@@ -101,28 +120,32 @@ int main(void) {
       check_game_over = 0;
     }
   }
-
-  // get eveything ready for ascii-art
   timer_stop(TIMER0);
-  term_clear_screen();
+}
 
-  // do the ascii-art
+// print ascii-art reporting win or loss
+void print_victoryscreen() {
+  term_clear_screen();
   switch (victory) {
   case 1:
-    term_println("//won-ascii-art");
+    art_print_victory();
     break;
   case 0:
-    term_println("//lost-ascii-art");
+    art_print_loss();
     break;
   }
+}
 
+// wait for user to continue to statistics, and print
+void print_stats() {
   // wait for user to continue to statistics
-  term_print("\n\n[press 'q' to continue to the statistics]");
+  term_print("\n[press 'q' to continue to the statistics]");
   while (term_stdin_read() != 'q') {
   }
 
   // print statistics
   term_clear_screen();
+  print_boards(&player, &bot, &cursor);
   int misses;
   int hits;
   misses = board_stats(&bot, MISS);
@@ -134,6 +157,8 @@ int main(void) {
   term_printnr(hits);
   term_print("\n shots: ");
   term_printnr(misses + hits);
+  term_println("\n");
+  art_print_bargraph(hits, misses);
 
   misses = board_stats(&player, MISS);
   hits = board_stats(&player, HIT);
@@ -144,6 +169,17 @@ int main(void) {
   term_printnr(hits);
   term_print("\n shots: ");
   term_printnr(misses + hits);
+  term_println("\n");
+  art_print_bargraph(hits, misses);
+}
 
+#pragma endregion
+
+int main(void) {
+  main_init();
+  select_dificulty();
+  main_game_loop();
+  print_victoryscreen();
+  print_stats();
   return 0;
 }
